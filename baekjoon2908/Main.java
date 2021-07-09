2f3d6cf83eac2fe8ba8fd82d459788c197fa45e7
Amy Lim <amylim32897@gmail.com>
2021-07-09 21:57:38 +0900

    Update Main.java

--- baekjoon2908/Main.java
+++ baekjoon2908/Main.java
@@ -8,14 +8,14 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-		String A = sc.next();
-		String B = sc.next();
+		String a = sc.next();
+		String b = sc.next();
 
-		StringBuffer a = new StringBuffer(A);
-		String reservedA = a.reverse().toString();
+		StringBuffer sb1 = new StringBuffer(a);
+		String reservedA = sb1.reverse().toString();
 
-		StringBuffer b = new StringBuffer(B);
-		String reservedB = b.reverse().toString();
+		StringBuffer sb2 = new StringBuffer(b);
+		String reservedB = sb2.reverse().toString();
 
 		if (Integer.parseInt(reservedA) > Integer.parseInt(reservedB)) {
 			System.out.println(reservedA);
