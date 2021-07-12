6df3eb1ebe749fed1c058458019a3e22f7a569d6
Amy Lim <amylim32897@gmail.com>
2021-07-12 17:52:09 +0900

    Create Main.java
    baekjoon1032

---
+++ baekjoon1032/Main.java
@@ -0,0 +1,34 @@
+package baekjoon1032;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		String[] filename = new String[n];
+		for (int i = 0; i < n; i++) {
+			filename[i] = sc.next();
+		}
+
+		boolean check;
+
+		for (int i = 0; i < filename[0].length(); i++) {
+			check = true;
+			for (int j = 0; j < n - 1; j++) {
+				if (filename[j].charAt(i) != filename[j + 1].charAt(i)) {
+					check = false;
+					break;
+				}
+			}
+			if (check)
+				System.out.print(filename[0].charAt(i));
+			else
+				System.out.print("?");
+		}
+
+	}
+}
