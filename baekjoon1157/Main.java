8968477e71ab439d39dc13fa4005be7a9d61032d
Amy Lim <amylim32897@gmail.com>
2021-07-14 12:19:49 +0900

    Update Main.java

--- baekjoon1157/Main.java
+++ baekjoon1157/Main.java
@@ -8,13 +8,11 @@ public class Main {
 		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 
-		String word = sc.next().toUpperCase(); // 대문자 출력
-		int[] alphabet = new int[26]; // A ~ Z
+		String word = sc.next().toUpperCase();
+		int[] alphabet = new int[26];
 
 		for (int i = 0; i < word.length(); i++) {
 			alphabet[word.charAt(i) - 'A']++;
-			// int[] alphabet = {0, 0, 0, 0, 0 .......};
-			// A면 인덱스 0의 값 증가 B면 인덱스 1의 값 증가 C면 인덱스 2의 값 증가
 		}
 		
 		int max = 0;
