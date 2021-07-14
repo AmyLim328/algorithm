3ab4856950a7cc4e4a9b5d7c8235996db349293e
Amy Lim <amylim32897@gmail.com>
2021-07-14 12:29:14 +0900

    Update Main.java

--- baekjoon2577/Main.java
+++ baekjoon2577/Main.java
@@ -6,16 +6,14 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
 		Scanner sc = new Scanner(System.in);
 
 		int a = sc.nextInt();
 		int b = sc.nextInt();
 		int c = sc.nextInt();
-
 		int mul = a * b * c;
-
 		int[] cnt = new int[10];
+		
 		for (int i = 0; i < String.valueOf(mul).length(); i++) {
 			cnt[String.valueOf(mul).charAt(i) - '0']++;
 		}
