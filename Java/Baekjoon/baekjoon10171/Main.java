f264c80d197c82840640faf95d576a66b875baaf
Amy Lim <amylim32897@gmail.com>
2021-07-24 22:20:48 +0900

    Create Main.java
    baekjoon10171

---
+++ Java/Baekjoon/baekjoon10171/Main.java
@@ -0,0 +1,17 @@
+package baekjoon10171;
+
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+
+	}
+}
